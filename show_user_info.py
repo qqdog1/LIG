39668f43019b3eb87f2e8bbb3ebf5d78eeb34c02
Shawn <qqdog1@yahoo.com.tw>
2021-03-05 17:55:10 +0800

    1. 顯示位置和需要經驗

--- show_user_info.py
+++ show_user_info.py
@@ -12,9 +12,8 @@ def lambda_handler(event, context):
 
     if 'Item' in db_result.keys():
         item = db_result['Item']
-        print(item)
-        result.append('角色名稱: ' + item['name'] + ' 所在區域: ' + str(item['location_id']))
-        result.append('等級: ' + str(item['lv']) + ' exp: ')
+        result.append('角色名稱: ' + item['name'] + ' 所在區域: ' + getLocationName(item['location_id']))
+        result.append('等級: ' + str(item['lv']) + ' exp: ' + str(item['exp']) + "/" + str(getNeedExp(item['lv'])))
         result.append('持有金錢: ' + str(item['money']))
         result.append('生命: ' + ' 魔力: ')
         result.append('攻擊: ' + ' 防禦: ')
@@ -25,3 +24,25 @@ def lambda_handler(event, context):
     json_dump = json.dumps(result, ensure_ascii=False)
 
     return bytes(json_dump, 'utf-8')
+
+
+def getLocationName(location_id):
+    table = dynamodb.Table('Location')
+    db_result = table.get_item(Key={'location_id': location_id})
+    if 'Item' in db_result.keys():
+        item = db_result['Item']
+        return item['name']
+    else:
+        print('找不到對應的location id:' + location_id)
+        return '???'
+
+
+def getNeedExp(lv):
+    table = dynamodb.Table('Level_Info')
+    db_result = table.get_item(Key={'lv': lv})
+    if 'Item' in db_result.keys():
+        item = db_result['Item']
+        return item['exp']
+    else:
+        print('找不到對應的exp:' + lv)
+        return '???'
